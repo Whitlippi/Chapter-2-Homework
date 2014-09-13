f81df9b2e0c7b8822709e7ae3e7059352d110c99
Whitlippi <whitlippi@gmail.com>
2014-09-13 17:48:11 -0400

    The programs for my Chapter 2 Homework
    Banner is for Question 15
    Double is for Question 11
    Penguin is for Question 9

---
+++ Banner.java
@@ -0,0 +1,54 @@
+/*
+ * This applet displays a black banner moving across the screen
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+import javax.swing.*;
+
+public class Banner extends JApplet
+	implements ActionListener
+{
+	private int xPos, yPos; // hold the coordinates of the banner
+	
+	public void init ()
+	{
+		Container c = getContentPane ();
+		c.setBackground(Color.WHITE);
+		xPos = c.getWidth ();
+		yPos = c.getHeight ();
+		Timer clock = new Timer(30, this); //Fires every 30 milliseconds
+		clock.start();
+	}
+	
+	//called automatically after repaint request
+	public void paint (Graphics g)
+	{
+		super.paint (g);
+		(g).setColor(Color.BLACK);
+		g.drawRect(0, 50, xPos, yPos);
+		g.fillRect(0, 50, xPos, yPos);
+	}
+	
+	// Called automatically when the timer fires 
+	public void actionPerformed (ActionEvent e)
+	{
+		Container c = getContentPane();
+		
+		// Adjust the horizontal position of the banner:
+		xPos--;
+		if (xPos < -100)
+		{
+			xPos = c.getWidth();
+		}
+		
+		//Set the vertical position of the banner:
+		yPos = c.getHeight() / 2;
+		
+		repaint();
+	}
+
+}
+
+
---
+++ Double.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kboard = new Scanner (System.in);
+		System.out.print ("Enter an integer: ");
+		int n = kboard.nextInt();
+		System.out.println("2 * " + n + " = " + (n+n));
+		System.out.println("If you change (n+n) to n+n you get this:");
+		System.out.println("2 * " + n + " = " + n + n);
+	}
+
+}
