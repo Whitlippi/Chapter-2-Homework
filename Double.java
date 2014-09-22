d6b3f34bb231477a89ab04d97c96a355a4d91b55
Ms. Kelly <juchkell@users.noreply.github.com>
2014-09-22 07:10:01 -0700

    Comments
    Full points

--- Double.java
+++ Double.java
@@ -10,6 +10,6 @@ public class Double {
 		System.out.println("2 * " + n + " = " + (n+n));
 		System.out.println("If you change (n+n) to n+n you get this:");
 		System.out.println("2 * " + n + " = " + n + n);
-	}
+	} //yes! And there are both a and b in your program, which is okay.
 
 }
